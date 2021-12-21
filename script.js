3e00b50dcda345dca9e5bf30a6ba70b6d6e3813f
Adam <94177926+okdemsky@users.noreply.github.com>
2021-12-21 18:01:05 -0500

    Add random generator functionality

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //write code here
-console.log("Hello World")
+const phraseArray = ['a', 'b', 'c']
+const photoArray = ['1','2','3']
+const songArray = ['$','!','&']
+
+//generate random quote
+function generateRandomPhrase(phraseArr) {
+    const rndPhrase = Math.floor(Math.random()*phraseArr.length);
+    const phraseToDisplay = phraseArr[rndPhrase];
+    
+    return phraseToDisplay;
+}
+//generate random photo
+function generateRandomPhoto(photoArr) {
+    const rndPhoto = Math.floor(Math.random()*photoArr.length);
+    const photoToDisplay = photoArr[rndPhoto];
+
+    return photoToDisplay;
+}
+//generate random song
+function generateRandomSong(songArr) {
+    const rndSong = Math.floor(Math.random()*songArr.length);
+    const songToDisplay = songArr[rndSong];
+
+    return songToDisplay;
+}
+
+function generateCombinedMessage () {
+    console.log(`${generateRandomPhrase(phraseArray)}, ${generateRandomPhoto(photoArray)}, ${generateRandomSong(songArray)}`)
+    
+}
+
+generateCombinedMessage()
