a9ca74af00de8504d3f401d25b65d82d97711a7d
Adam <94177926+okdemsky@users.noreply.github.com>
2021-12-22 13:53:17 -0500

    Comment out photo and song

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //write code here
 const dailyVibe = {
-    "phrase": ['a', 'b', 'c'],
-    "photo": ['1','2','3'],
-    "song": ['$','!','&']
+    "phrase": ['Success is not final; failure is not fatal: it is the courage to continue that counts. – Winston Churchill', 'Play by the rules, but be ferocious. – Phil Knight', 'Business opportunities are like buses, there’s always another one coming. – Richard Branson', 'Every problem is a gift—without problems we would not grow. – Anthony Robbins', 'You only have to do a few things right in your life so long as you don’t do too many things wrong. – Warren Buffett', 'Success usually comes to those who are too busy to be looking for it. – Henry David Thoreau', 'And the day came when the risk to remain tight in a bud was more painful than the risk it took to blossom. – Anaïs Nin', 'There’s no shortage of remarkable ideas, what’s missing is the will to execute them. – Seth Godin', 'I owe my success to having listened respectfully to the very best advice, and then going away and doing the exact opposite. – G.K. Chesterton', 'I don’t know the word ‘quit.’ Either I never did, or I have abolished it. – Susan Butcher', 'Far and away the best prize that life offers is the chance to work hard at work worth doing. – Theodore Roosevelt', 'If you really look closely, most overnight successes took a long time. – Steve Jobs', 'Almost everything worthwhile carries with it some sort of risk, whether it’s starting a new business, whether it’s leaving home, whether it’s getting married, or whether it’s flying into space. – Chris Hadfield', 'Even if you are on the right track, you’ll get run over if you just sit there. – Will Rodgers', 'The real test is not whether you avoid this failure, because you won’t. It’s whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere. – Barack Obama', 'Forget past mistakes. Forget failures. Forget everything except what you’re going to do now and do it. – William Durant', 'Imagination is everything. It is the preview of life’s coming attractions. – Albert Einstein'],
+
+    // "photo": ['1','2','3'],
+    // "song": ['$','!','&']
 }
 const rndPhrase = Math.floor(Math.random()*dailyVibe["phrase"].length);
-const rndPhoto = Math.floor(Math.random()*dailyVibe["photo"].length);
-const rndSong = Math.floor(Math.random()*dailyVibe["song"].length);
+// const rndPhoto = Math.floor(Math.random()*dailyVibe["photo"].length);
+// const rndSong = Math.floor(Math.random()*dailyVibe["song"].length);
 
 const phraseEl = document.getElementById("phrase")
-const photoEl = document.getElementById("photo")
-const songEl = document.getElementById("song")
+// const photoEl = document.getElementById("photo")
+// const songEl = document.getElementById("song")
 
 function dailyVibeIterator (obj) {
     for (let i in obj) {
@@ -19,14 +20,14 @@ function dailyVibeIterator (obj) {
                 let newPhrase = obj[i][rndPhrase]
                 phraseEl.innerHTML = newPhrase;
                 break;
-            case "photo":
-                let newPhoto = obj[i][rndPhoto]
-                photoEl.innerHTML = newPhoto;
-                break;
-            case "song":
-                let newSong = obj[i][rndSong]
-                songEl.innerHTML = newSong;
-                break;
+            // case "photo":
+            //     let newPhoto = obj[i][rndPhoto]
+            //     photoEl.innerHTML = newPhoto;
+            //     break;
+            // case "song":
+            //     let newSong = obj[i][rndSong]
+            //     songEl.innerHTML = newSong;
+            //     break;
 
                 
 
@@ -36,12 +37,13 @@ function dailyVibeIterator (obj) {
     
 }
 
+
 dailyVibeIterator(dailyVibe)
 
 
-const phraseArray = ['a', 'b', 'c']
-const photoArray = ['1','2','3']
-const songArray = ['$','!','&']
+// const phraseArray = ['a', 'b', 'c']
+// const photoArray = ['1','2','3']
+// const songArray = ['$','!','&']
 
 //generate random quote
 // function generateRandomPhrase(phraseArr) {
